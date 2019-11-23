e5b27c1e69057f3c843243f68f2d72cf3ef3bb7a
Oleg-Melnikow <melnikow.olezhka@mail.ru>
2019-11-23 15:55:12 +0300

    add socket

--- src/App.js
+++ src/App.js
@@ -3,19 +3,25 @@ import './App.css'
 import Chats from './components/Chats/Chats'
 import CurrentChat from './components/CurrentChat/CurrentChat'
 import {api} from "./DAL/Api";
+import openSocket from "socket.io";
 
 class App extends React.Component {
-    componentDidMount() {
-        debugger
-        api.getToken().then((res)=>{
-            debugger;
+    async componentDidMount() {
+       /* debugger*/
+        await api.getToken().then((res)=>{
+            localStorage.setItem('token', res.data)
+            /*debugger;*/
         })
+        const socket = openSocket('http://messenger-hackathon.herokuapp.com');
+        socket.on('get-chats-success', (chats)=>{console.log(chats)});
+        socket.emit("get-chats", {token: localStorage.getItem("token")})
     }
 
     render() {
     return (
       <div className="App">
         <Chats />
+
         <CurrentChat />
       </div>
     )
