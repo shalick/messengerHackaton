1e67051db00518944b5475ce1be7c08fcf2ea9a8
Anastasia Pyzhik <chizhikpyzhik2015@gmail.com>
2019-11-23 13:14:46 +0300

    qqqq

--- src/components/Chats/Chat/Chat.jsx
+++ src/components/Chats/Chat/Chat.jsx
@@ -5,6 +5,7 @@ class Chat extends React.Component {
     render() {
         return (
             <div className={s.chat}>
+            <h1>wqwe</h1>
                 
             </div>
         )
