9dd792e76ceb0a7b2ca56b56b24b140209cb6b92
Anastasia Pyzhik <chizhikpyzhik2015@gmail.com>
2019-11-23 13:12:12 +0300

    Add package-lock to ignor

---
+++ src/components/Chats/Chat/Chat.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import s from './Chat.module.css'
+
+class Chat extends React.Component {
+    render() {
+        return (
+            <div className={s.chat}>
+                
+            </div>
+        )
+    }
+}
+
+
+export default Chat
