59f159d4352bd02cc07e42eb365920c229bbd616
Oleg-Melnikow <melnikow.olezhka@mail.ru>
2019-11-23 14:04:10 +0300

    add api

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import React from 'react'
 import './App.css'
 import Chats from './components/Chats/Chats'
 import CurrentChat from './components/CurrentChat/CurrentChat'
+import {api} from "./DAL/Api";
 
 class App extends React.Component {
-  render() {
+    componentDidMount() {
+        debugger
+        api.getToken().then((res)=>{
+            debugger;
+        })
+    }
+
+    render() {
     return (
       <div className="App">
         <Chats />
